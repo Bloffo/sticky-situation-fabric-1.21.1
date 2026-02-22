b54dc159c7a257e08ebd48228cca04a6d8bfbc8b
webblepebbles <226301485+webblepebbles@users.noreply.github.com>
2026-02-22 19:09:48 +1100

    stik 1.21.11

--- src/main/java/net/bloffo/stickysituation/item/ModItems.java
+++ src/main/java/net/bloffo/stickysituation/item/ModItems.java
@@ -6,18 +6,24 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item FANCY_STICK =registerItem("fancy_stick", new Item(new Item.Settings()));
-    public static final Item BIG_STICK =registerItem("big_stick", new Item(new Item.Settings()));
-    public static final Item BIGGER_STICK =registerItem("bigger_stick", new Item(new Item.Settings()));
-    public static final Item EVEN_BIGGER_STICK =registerItem("even_bigger_stick", new Item(new Item.Settings()));
-    public static final Item BIGGEST_STICK =registerItem("biggest_stick", new Item(new Item.Settings()));
-
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(StickySituation.MOD_ID, name), item);
+    public static final Item FANCY_STICK =registerItem("fancy_stick");
+    public static final Item BIG_STICK =registerItem("big_stick");
+    public static final Item BIGGER_STICK =registerItem("bigger_stick");
+    public static final Item EVEN_BIGGER_STICK =registerItem("even_bigger_stick");
+    public static final Item GIANT_STICK =registerItem("giant_stick");
+    public static final Item ENORMOUS_STICK =registerItem("enormous_stick");
+    public static final Item BIGGEST_STICK =registerItem("biggest_stick");
+    private static Item registerItem(String name) {
+        Identifier id = Identifier.of(StickySituation.MOD_ID, name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        return Registry.register(Registries.ITEM, key, new Item(new Item.Settings().registryKey(key)));
     }
+
     public static void registerModItems() {
         StickySituation.LOGGER.info("Registering mod items for " + StickySituation.MOD_ID);
 
@@ -26,6 +32,8 @@ public class ModItems {
             entries.add(BIG_STICK);
             entries.add(BIGGER_STICK);
             entries.add(EVEN_BIGGER_STICK);
+            entries.add(GIANT_STICK);
+            entries.add(ENORMOUS_STICK);
             entries.add(BIGGEST_STICK);
         });
     }
